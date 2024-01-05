f1c0fd6a563933fbd900cf559da6a74eef4ba404
Enrique Torres <etochavez@gmail.com>
2024-01-05 10:36:58 -0500

    Added new endpoint to create db structure.

--- shoppingCartService/main.py
+++ shoppingCartService/main.py
@@ -1,14 +1,32 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Depends, HTTPException
 
 from db.sessions import create_tables
-
+from domain.exceptions.entity_does_not_exist import EntityDoesNotExist
+from domain.repository.cart_repository import CartRepository
+from infraestructure.dependencies.repository import get_repository
+from infraestructure.schemas.cart_items import CartItemRead, CartItemCreate
 
 app = FastAPI()
 
 
-@app.post("/cart/additem")
-async def add_item(item: CartItem):
-    return "Item added"
+@app.post(
+    "/cart/addItem",
+    status_code=status.HTTP_201_CREATED,
+    response_model=CartItemRead,
+    name="add item"
+)
+async def add_item(
+        item: CartItemCreate,
+        cart_repository: CartRepository = Depends(get_repository(CartRepository))
+):
+    try:
+        cart = await cart_repository.get(item.user_id)
+    except EntityDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cart not found!"
+        )
+
+    return item.__dict__
     # if item.user_id not in carts_db:
     #     carts_db[item.user_id] = ShoppingCart(user_id=item.user_id, items=[])
     #
@@ -26,7 +44,6 @@ async def add_item(item: CartItem):
     #     raise HTTPException(status_code=400, detail="Insufficient inventory")
 
 
-
-# @app.get("/init_tables", status_code=status.HTTP_200_OK, name="init_tables")
-# async def init_tables():
-#     create_tables()
+@app.get("/init_tables", status_code=status.HTTP_200_OK, name="init_tables")
+async def init_tables():
+    create_tables()
